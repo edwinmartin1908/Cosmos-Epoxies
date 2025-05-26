ae9636cd347498691025b13d06ff731651837e5e
edwinmartin1908 <emartin@pucp.pe>
2025-05-26 16:17:45 -0500

    Add files via upload

---
+++ blogprueba.js
@@ -0,0 +1,410 @@
+let articles={
+    data:[{
+        id:1,
+        name:"How Epoxy Coatings Protect Against Corrosion",
+        category:"anticorrosion",
+        image: "images/blog1.jpg",    
+    },
+    {
+        id:2,
+        name:"How Sacrificial Anode Cathodic Protection Works in Epoxy Coatings for Corrosion Prevention",
+        category:"anticorrosion",
+        image: "articles images/How Sacrificial Anode Cathodic Protection Works in Epoxy Coatings for Corrosion Prevention.avif",    
+    },
+    {   
+        id:3,
+        name:"How Epoxy Coatings Provide a Physical Barrier Against Particle Infiltration and Contamination",
+        category:"anticorrosion",
+        image: "articles images/How Epoxy Coatings Provide a Physical Barrier Against Particle Infiltration and Contamination.png",    
+    },
+    {
+        id:4,
+        name:"Corrosion Patterns Explained: Types, Causes, and Prevention in Coatings and Metals",
+        category:"anticorrosion",
+        image: "articles images/Forms-of-corrosion-1.jpg",    
+    },
+    {
+        id:5,
+        name:"Why Epoxy Resins Offer Excellent Chemical Resistance: Key Factors and Benefits",
+        category:"chemicalResistance",
+        image: "articles images/chemical-resistance.webp",    
+    },
+    {
+        id:6,
+        name:"Chemical Groups Vulnerable to Chemical Attacks in Epoxy Coatings: Protecting Against Degradation",
+        category:"chemicalResistance",
+        image: "articles images/chemical-groups.jpg",    
+    },
+    {
+        id:7,
+        name:"Common Coating Defects in Epoxy Paints: Types, Causes, and How to Prevent Them",
+        category:"mechanicalResistance",
+        image: "articles images/coatings-defects.jpg",    
+    },
+    {
+        id:8,
+        name:"Understanding Mechanical Stresses in Epoxy Coatings: Types, Effects, and How to Mitigate Them",
+        category:"mechanicalResistance",
+        image: "articles images/mechanical-stresses.jpg",    
+    },
+    {
+        id:9,
+        name:"Elastic vs Plastic Deformation in Epoxy Coatings: Understanding the Key Differences for Enhanced Durability",
+        category:"mechanicalResistance",
+        image: "articles images/elastic-plastic-deformation.jpg",    
+    },
+    {
+        id:10,
+        name:"Hardness in Epoxy Coatings: How It Affects Durability, Resistance, and Long-Term Performance",
+        category:"mechanicalResistance",
+        image: "articles images/hardness.jpg",    
+    },
+    {   
+        id:11,
+        name:"Toughness in Epoxy Coatings: Understanding Tenacity and How It Improves Durability and Performance",
+        category:"mechanicalResistance",
+        image: "articles images/toughness.jpg",    
+    },
+    {
+        id:12,
+        name:"What is Temperature",
+        category:"highTemperatures",
+        image: "articles images/high-temperature.avif",    
+    },
+    {
+        id:13,
+        name:"Drying Additives in Epoxy Coatings: How They Accelerate Curing Time and Improve Efficiency",
+        category:"additives",
+        image: "articles images/drying.png",    
+    },
+    {
+        id:14,
+        name:"Antifoaming Additives in Epoxy Coatings: How They Prevent Foam and Improve Surface Quality",
+        category:"additives",
+        image: "articles images/foaming.jpg",    
+    },
+    {
+        id:15,
+        name:"Wetting Additives in Epoxy Coatings: Function, Benefits, and How They Improve Surface Bonding and Spread",
+        category:"additives",
+        image: "articles images/wetting.jpg",    
+    },
+    {
+        id:16,
+        name:"Leveling Additives in Epoxy Coatings: Function, Benefits, and How They Ensure a Smooth Finish",
+        category:"additives",
+        image: "articles images/leveling.png",    
+    },
+    {
+        id:17,
+        name:"Thickener Additives in Epoxy Coatings: Purpose, Benefits, and How They Improve Application Control",
+        category:"additives",
+        image: "articles images/dispersants.jpg",    
+    },
+    {
+        id:18,
+        name:"Dispersant Additives in Epoxy Coatings: Function, Benefits, and Why Pigment Stability Matters",
+        category:"additives",
+        image: "articles images/dispersants.jpg",    
+    },
+    {
+        id:19,
+        name:"Antisedimentation Additives in Epoxy Coatings: Benefits, Function, and How They Prevent Pigment Settling",
+        category:"additives",
+        image: "articles images/sedimentation.jpg",    
+    },
+    {
+        id:20,
+        name:"Plasticizer Additives in Epoxy Coatings: Benefits, Applications, and How They Improve Flexibility",
+        category:"additives",
+        image: "articles images/plasticizer.png",    
+    },
+    {
+        id:21,
+        name:"Oxidizing Additives in Epoxy Coatings: Functions, Benefits, and When to Use Them",
+        category:"additives",
+        image: "articles images/oxidatives.jpg",    
+    },
+    {
+        id:22,
+        name:"Phenalkamine Hardeners for Epoxy Coatings: Benefits, Applications, and Moisture Tolerance Explained",
+        category:"chemicalResistance",
+        image: "articles images/phenalkamine.png",    
+    },
+    {
+        id:23,
+        name:"Anhydride Hardeners in Epoxy Coatings: Advantages, Applications, and How They Compare",
+        category:"chemicalResistance",
+        image: "articles images/anhydride.png",    
+    },
+    {
+        id:24,
+        name:"Aliphatic vs. Aromatic Amine Hardeners: Key Differences and How They Affect Epoxy Coatings",
+        category:"hardeners",
+        image: "articles images/aliphatic-aromatic.webp",    
+    },
+    {
+        id:25,
+        name:"Getting to Know the Epoxy Prepolymer: DGEBA",
+        category:"resins",
+        image: "articles images/DGEBA.png",    
+    },
+    {
+        id:26,
+        name:"Novolac Epoxy Resin",
+        category:"resins",
+        image: "articles images/novolac.jpg",    
+    },
+    {
+        id:27,
+        name:"What Are Epoxy Ester Resins? Benefits and Uses in Industrial Protective Coatings",
+        category:"resins",
+        image: "articles images/epoxy-resins-benefits.jpg",    
+    },
+    {
+        id:28,
+        name:"Alkylamine Phenolic Epoxy Coatings: Benefits, Applications, and Why They're Used in Harsh Environments",
+        category:"hardeners",
+        image: "articles images/alkylamine phenolic.png",    
+    },
+    {
+        id:29,
+        name:"Amine vs. Amide Hardeners: Key Differences and Uses in Epoxy Coatings",
+        category:"hardeners",
+        image: "articles images/amine-amide.webp",    
+    },
+    {
+        id:30,
+        name:"Technical Guide: Surface Preparation Methods for Metallic Substrates Prior to Epoxy Coating",
+        category:"substratePretreatment",
+        image: "articles images/surfaceprep.jpg",    
+    },
+    {
+        id:31,
+        name:"Anchor Profile: The Critical Role of Surface Roughness in Epoxy Coating Adhesion",
+        category:"substratePretreatment",
+        image: "articles images/surfacerough.webp",    
+    },
+    {
+        id:32,
+        name:"Invisible Contaminants: How Soluble Salts, Oils, and Moisture Undermine Epoxy Coating Adhesion",
+        category:"substratePretreatment",
+        image: "articles images/metalcontaminants.webp",    
+    },
+    {
+        id:33,
+        name:"International Standards for Metal Surface Preparation: ISO, SSPC, and NACE Explained",
+        category:"substratePretreatment",
+        image: "articles images/ampp.png",    
+    },
+    {
+        id:34,
+        name:"Common Surface Preparation Mistakes and How to Avoid Them",
+        category:"substratePretreatment",
+        image: "articles images/prepmistakes.webp",    
+    },
+    {
+        id:35,
+        name:"What Are Epoxy Coatings for Marine Environments?",
+        category:"marineEnvironment",
+        image: "articles images/marine-environment.jpg",    
+    },
+    {
+        id:36,
+        name:"The Role of Epoxy Coatings in Preventing Corrosion in Marine Environments",
+        category:"marineEnvironment",
+        image: "articles images/marine.jpg",    
+    },
+    {
+        id:37,
+        name:"The Benefits of Epoxy Coatings for Offshore Platforms",
+        category:"marineEnvironment",
+        image: "articles images/marine2.webp",    
+    },
+    {
+        id:38,
+        name:"How Epoxy Coatings Improve the Lifespan of Ship Hulls",
+        category:"marineEnvironment",
+        image: "articles images/marine3.jpg",    
+    },
+    {
+        id:39,
+        name:"What Makes Epoxy Coatings Heat Resistant?",
+        category:"highTemperatures",
+        image: "articles images/heat1.webp",    
+    },
+    {
+        id:40,
+        name:"Why High-Temperature Epoxy Coatings Matter in Industrial Maintenance",
+        category:"highTemperatures",
+        image: "articles images/heat2.jpg",    
+    },
+    {
+        id:41,
+        name:"Key Applications of High-Temperature Epoxy Coatings",
+        category:"highTemperatures",
+        image: "articles images/heat3.jpeg",    
+    },
+    {
+        id:42,
+        name:"How to Choose the Right High-Temperature Epoxy Coating",
+        category:"highTemperatures",
+        image: "articles images/heat4.webp",    
+    },
+    {
+        id:43,
+        name:"The Role of Nanotechnology in Epoxy Coatings",
+        category:"newTechnologies",
+        image: "articles images/nanotechnology.jpg",    
+    },
+    {
+        id:44,
+        name:"What is Self-Healing Epoxy Coatings?",
+        category:"newTechnologies",
+        image: "articles images/selfhealing.jpg",    
+    },
+    {
+        id:45,
+        name:"High-Performance Epoxy Coatings with Hybrid Resins",
+        category:"newTechnologies",
+        image: "articles images/hybrid.jpg",    
+    },
+    {
+        id:46,
+        name:"The Future of Epoxy Coatings: Eco-Friendly and Low-VOC Innovations",
+        category:"newTechnologies",
+        image: "articles images/future.jpg",    
+    },
+     {
+        id:47,
+        name:'Cosmos Guide 1',
+        category:"guides",
+        image: "articles images/future.jpg",    
+    },
+     {
+        id:48,
+        name:"Cosmos Guide 2",
+        category:"guides",
+        image: "articles images/future.jpg",    
+    },
+     {
+        id:49,
+        name:"Cosmos Guide 3",
+        category:"guides",
+        image: "articles images/future.jpg",    
+    },
+    {
+        id:50,
+        name:"Cosmos Guide 4",
+        category:"guides",
+        image: "articles images/future.jpg",    
+    }
+]
+};
+
+  const itemsPerPage = 6;
+  let currentPage = 1;
+  let currentCategory = "All";
+  const grid = document.querySelector(".blog-section__grid");
+  const pagination = document.querySelector(".blog-section__pagination");
+  const pdfDownloadIds = [47, 48, 49, 50];
+
+  // Render all cards (hidden or no) once
+  function createAllCards() {
+    articles.data.forEach((item, index) => {
+      const ancle = document.createElement("a");
+      ancle.setAttribute("href", `article.html`);
+      ancle.setAttribute("data-id", item.id);
+      ancle.classList.add("blog-card", item.category);
+      const card = document.createElement("div");
+      card.classList.add("blog-card-card");
+      const imgContainer = document.createElement("div");
+      const image = document.createElement("img");
+      const name = document.createElement("h4");
+
+      imgContainer.classList.add("card-img");
+      image.setAttribute("src", item.image);
+      name.classList.add("card-title");
+      name.innerText = item.name;
+
+      imgContainer.appendChild(image);
+      card.appendChild(imgContainer);
+      card.appendChild(name);
+      ancle.appendChild(card);
+      grid.appendChild(ancle);
+
+      ancle.addEventListener("click", function () {
+        localStorage.setItem("articleId", this.dataset.id);
+      });
+    });
+  }
+
+  function FilterProduct(category) {
+    const cards = document.querySelectorAll(".blog-card");
+    let filtered = [];
+
+    cards.forEach(card => {
+      if (category === "All" || card.classList.contains(category)) {
+        card.classList.remove("hide");
+        filtered.push(card);
+      } else {
+        card.classList.add("hide");
+      }
+    });
+
+    paginate(filtered);
+  }
+
+ 
+function paginate(filteredCards) {
+    const totalPages = Math.ceil(filteredCards.length / itemsPerPage);
+    pagination.innerHTML = ""; // Limpiar contenido de la paginación
+  
+    // Ocultar todos los artículos primero
+    filteredCards.forEach((card) => {
+      card.classList.add("hide");
+    });
+  
+    // Mostrar solo los artículos correspondientes a la página actual
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    filteredCards.slice(start, end).forEach((card) => {
+      card.classList.remove("hide");
+    });
+  
+    // Crear los botones de paginación
+    for (let i = 1; i <= totalPages; i++) {
+      const btn = document.createElement("button");
+      btn.classList.add("pagination-button");
+      btn.innerText = i;
+      if (i === currentPage) {
+        btn.disabled = true; // Deshabilitar el botón de la página actual
+      }
+  
+      btn.addEventListener("click", () => {
+        currentPage = i;
+        paginate(filteredCards); // Recargar los artículos para la página seleccionada
+      });
+  
+      pagination.appendChild(btn); // Agregar el botón de paginación al contenedor
+    }
+  }
+
+  function setupFilters() {
+    const checkboxes = document.querySelectorAll("input[type='checkbox']");
+    checkboxes.forEach(checkbox => {
+      checkbox.addEventListener("change", () => {
+        checkboxes.forEach(cb => cb.checked = false);
+        checkbox.checked = true;
+        currentCategory = checkbox.value;
+        currentPage = 1;
+        FilterProduct(currentCategory);
+      });
+    });
+  }
+
+  window.onload = () => {
+    createAllCards();
+    setupFilters();
+    FilterProduct("All");
+  };
