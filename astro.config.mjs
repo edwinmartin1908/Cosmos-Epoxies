3e36b99f10a90f08192d0c5d468bd60a284b3dcb
Edwin Martin <edwinmartin1908@gmail.com>
2026-03-24 13:00:25 -0500

    probando quitarle cosas al config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://edwinmartin1908.github.io",
+  base: "/Cosmos-Coatings/",
   vite: {
     plugins: [tailwindcss()]
   },
